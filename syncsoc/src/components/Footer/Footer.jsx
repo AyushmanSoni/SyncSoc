4812d7724f863b14088e9e7e68d2c65f95b38804
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-08-03 01:54:12 +0530

    completed footer desing

--- syncsoc/src/components/Footer/Footer.jsx
+++ syncsoc/src/components/Footer/Footer.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='px-24 py-8 bg-[#2E1A12] text-white'>
+      <div className='flex flex-col md:flex-row justify-between items-start'>
+        <div>
+          <div className='flex items-center'>
+            <img className='h-10 me-4' src='https://www.svgrepo.com/show/458732/group.svg' alt='logo' style={{ filter: 'invert(1)' }} />
+            <h1 className='text-3xl font-semibold text-white'>
+              Sync<span className='text-white'>Soc</span>
+            </h1>
+          </div>
+          <p className="text-[16px] text-[#b7afa6] font-light tracking-normal leading-6 mt-2 sm:w-full" style={{ fontFamily: ["Open Sans Condensed Light", "sans-serif"] }}>
+            Unite Innovate and Succeed Together
+          </p>
+          <div className='mt-20'>
+            <h2 className='font-medium mb-2 text-gray-300'>Contact Us</h2>
+            <p>SyncSoc@gmail.com</p>
+          </div>
+        </div>
+        <div className='text-center md:text-left'>
+          <h2 className='font-semibold mb-2 text-gray-300'>Cultural Societies</h2>
+          <ul className='text-[14px] font-light text-[#CDBFB1]'>
+            <li className='mb-2'><a href='https://www.instagram.com/cultural_soc1' target='_blank' rel='noopener noreferrer'>Rangtarangini</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/cultural_soc2' target='_blank' rel='noopener noreferrer'>GeneticX Crew</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/cultural_soc3' target='_blank' rel='noopener noreferrer'>Viruosi</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/cultural_soc4' target='_blank' rel='noopener noreferrer'>Saraswa</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/cultural_soc5' target='_blank' rel='noopener noreferrer'>Nirmiti</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/cultural_soc6' target='_blank' rel='noopener noreferrer'>AMS</a></li>
+          </ul>
+        </div>
+        <div className='text-center md:text-left'>
+          <h2 className='font-semibold mb-2 text-gray-300'>Technical Societies</h2>
+          <ul className='text-[14px] font-light text-[#CDBFB1]'>
+            <li className='mb-2'><a href='https://www.instagram.com/technical_soc1' target='_blank' rel='noopener noreferrer'>Geekhaven</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/technical_soc2' target='_blank' rel='noopener noreferrer'>Gravity</a></li>
+            <li className='mb-2'><a href='https://www.instagram.com/technical_soc3' target='_blank' rel='noopener noreferrer'>Tesla</a></li>
+          </ul>
+        </div>
+        <div className='text-center md:text-left'>
+          <h2 className='font-semibold mb-2 text-gray-300'>Sports Society</h2>
+          <ul className='text-[14px] font-light text-[#CDBFB1]'>
+            <li><a href='https://www.instagram.com/sports_soc' target='_blank' rel='noopener noreferrer'>Spirit</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className='mt-8 text-center'>
+        <h1 className='text-0.5xl font-medium'>
+          &copy; 2024, Made with love in IIITA
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- syncsoc/src/components/Navbar/Navbar.jsx
+++ syncsoc/src/components/Navbar/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
     <div className='bg-[#FFFDFB] text-[#F9F6F3] px-4 md:px-24 py-4'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center'>
-          <img className='h-10 me-4' src='https://www.svgrepo.com/show/458732/group.svg' alt='logo' />
-          <h1 className='text-2xl font-semibold text-[#086D8A]'>
-            Sync<span className='text-[#032B37]'>Soc</span>
+          <img className='h-10 me-4 text-[#2E1A12]' src='https://www.svgrepo.com/show/458732/group.svg' alt='logo' />
+          <h1 className='text-2xl font-semibold text-[#683B2B]'>
+            Sync<span className='text-[#2E1A12]'>Soc</span>
           </h1>
         </div>
         <button
@@ -29,7 +29,7 @@ const Navbar = () => {
           <FaGripLines />
         </button>
         <div className={`hidden md:flex md:flex-grow md:justify-center md:items-center`}>
-          <div className='flex flex-col md:flex-row md:gap-8 text-[#F9F6F3]'>
+          <div className='flex flex-col md:flex-row md:gap-8 text-[#683B2B] font-medium hover:text-[#2E1A12]'>
             {links.map((item, i) => (
               <div key={i} className='px-4 py-2 '>
                 {item.title}
@@ -38,10 +38,10 @@ const Navbar = () => {
           </div>
         </div>
         <div className='hidden md:flex md:items-center md:gap-8'>
-          <div className='px-4 py-2 text-[#086D8A] hover:text-[#075A71]'>
+          <div className='px-4 py-2 text-[#683B2B] font-medium hover:text-[#2E1A12]'>
             LogIn
           </div>
-          <div className='px-4 py-2 bg-[#086D8A] text-white border rounded-lg hover:bg-[#075A71]'>
+          <div className='px-4 py-2 bg-[#683B2B] text-white border rounded-lg hover:bg-[#2E1A12]'>
             SignUp
           </div>
         </div>
