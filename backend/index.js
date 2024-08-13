2ce59798401cb7fb36de6faa1e4ed0ab52f8e4b7
Siddharth Singh <siddharth.singh022004@gmail.com>
2024-08-14 03:02:22 +0530

    CONNECTION CHANGES

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 
 // constants 
-const port = 5000
+ const port = 5000
 const cors = require("cors");
 app.use(cors());
+
+require("dotenv").config();
+
+// const PORT = process.env.PORT || 5000;
 
 // predefined middleware
 app.use(express.urlencoded({ extended: false}))
